7e36ba79cceafdacc0ac9da8e88bef7f59a6b86b
إسحاق <admin@admins-air.home>
2023-11-07 00:56:44 +0000

    Added form to contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./contact.scss";
 
 const Contact = () => {
+
+  const [message,setMessage] = useState(false)
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    setMessage(true);
+  }
   return (
-    <div className="contact" id="contact">Contact</div>
+    <div className="contact" id="contact">
+      <div className="left">
+
+      </div>
+      <div className="right">
+        <h2>Contact</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email"/>
+          <textarea placeholder="Messdage"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thanks, I'll get back to you as soon as I can =)</span>}
+        </form>
+      </div>
+    </div>
   )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./portfolio.scss"
+import PortfolioList from '../portfolioList/PortfolioList';
 const Portfolio = () => {
+
+  const [selected,setSelected] = useState("featured");
+
+  const list = [
+    {
+      id: "featured",
+      title : "Featured",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "game",
+      title: "Game App"
+    },
+    {
+      id: "mobile",
+      title: "Mobile App"
+    },
+
+  ];
+  
+
   return (
-    <div className="portfolio" id="portfolio">Portfolio</div>
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map(item =>(
+          //active set to true if item is selected
+          <PortfolioList title ={item.title} active ={selected === item.id} setSelected ={setSelected} id={item.id}/>
+        ))}
+      </ul>
+      <div className="portfolioItemContainer">
+        <div className="item">
+          <img src="https://staticg.sportskeeda.com/editor/2022/08/53e15-16596004347246.png?w=840" alt="" />
+          <h3>Naruto Web App</h3>
+        </div>
+        <div className="item">
+          <img src="https://staticg.sportskeeda.com/editor/2022/08/53e15-16596004347246.png?w=840" alt="" />
+          <h3>Naruto Web App</h3>
+        </div>
+        <div className="item">
+          <img src="https://staticg.sportskeeda.com/editor/2022/08/53e15-16596004347246.png?w=840" alt="" />
+          <h3>Naruto Web App</h3>
+        </div>
+        <div className="item">
+          <img src="https://staticg.sportskeeda.com/editor/2022/08/53e15-16596004347246.png?w=840" alt="" />
+          <h3>Naruto Web App</h3>
+        </div>
+      </div>
+    </div>
   )
 }
 
